20228b4baaf916bead9da86e8ff8ed03ac23597d
Ubuntu <ubuntu@ip-172-31-25-140.ec2.internal>
2025-05-29 20:07:20 +0000

    Commiting the changes to save my progress and also practicing how to commit from ubuntu

---
+++ Day02/characters.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+<< Multicomments 
+Now you can type anything here as this is comment block named as Multicomments. We can type
+multiple lines and we dont have to worry about putting a hash on every line.     I am psuhing this file to github for bash scripting practice for devops
+Multicomments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name="Jeff"
+echo "My name is $name and date is $(date)"
+
